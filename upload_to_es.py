11e861dc35eb226d8bbd793f4cb7f84ed080dca0
Jay Miller <kjaymiller@gmail.com>
2021-06-16 14:36:10 -0700

    upload with no location data

--- upload_to_es.py
+++ upload_to_es.py
@@ -1,7 +1,22 @@
-import os
 import eland as ed
 import pandas as pd
 from elasticsearch import Elasticsearch
+from connection import client
 
-df = pd.read_csv("fatal-police-shootings-data.csv")
-ed.pandas_to_eland(df, es_index="wapo-police-shootings-data", if_es_exists="replace")
+def set_location(x):
+    location = ",".join(x.values.astype(str))
+    return location
+
+df = pd.read_csv(
+        "fatal-police-shootings-data.csv",
+        parse_dates=True,
+        )
+
+def upload():
+    ed.pandas_to_eland(
+            df,
+            es_client=client,
+            es_dest_index="wapo-police-shootings-data",
+            es_dropna=True,
+            es_if_exists="replace",
+            )
