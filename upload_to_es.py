3d88e6b81b231bb33b94c167b64c0300dafed036
Jay Miller <kjaymiller@gmail.com>
2021-06-16 13:22:53 -0700

    add eland and pandas upload

--- upload_to_es.py
+++ upload_to_es.py
@@ -1,6 +1,7 @@
 import os
-import eland
-import pandas
+import eland as ed
+import pandas as pd
 from elasticsearch import Elasticsearch
 
-pass
+df = pd.read_csv("fatal-police-shootings-data.csv")
+ed.pandas_to_eland(df, es_index="wapo-police-shootings-data", if_es_exists="replace")
